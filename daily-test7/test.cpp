44ff35435cb8a1cbf7eb363ace337b5787774def
wlkkt <yangyf0618@qq.com>
2024-07-06 21:09:09 +0800

    完善

--- daily-test7/test.cpp
+++ daily-test7/test.cpp
@@ -145,3 +145,30 @@
 //    printf("process quit normal\n");
 //    return 0;
 //}
+
+
+//#include <iostream>
+//#include <pthread.h>
+//#include <unistd.h>
+//#include <sys/types.h>
+//
+//void* newthreadrun(void* args)
+//{
+//    while (true)
+//    {
+//        std::cout << "I am new thread, pid: " << getpid() << std::endl;
+//        sleep(1);
+//    }
+//}
+//
+//int main()
+//{
+//    pthread_t tid;
+//    pthread_create(&tid, nullptr, newthreadrun, nullptr);
+//
+//    while (true)
+//    {
+//        std::cout << "I am main thread, pid: " << getpid() << std::endl;
+//        sleep(1);
+//    }
+//}
