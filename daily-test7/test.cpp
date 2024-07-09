8e629a64e696d50967b1c2d3747782f044b9112c
wlkkt <yangyf0618@qq.com>
2024-07-09 22:53:15 +0800

    完善

--- daily-test7/test.cpp
+++ daily-test7/test.cpp
@@ -377,3 +377,85 @@ int main()
 //     std::cout << "main thread quit, n=" << n << " main thread get a ret: " << (long long)ret << std::endl;
 //     return 0;
 // }
+
+
+#ifndef __THREAD_HPP__
+#define __THREAD_HPP__
+
+#include <iostream>
+#include <string>
+#include <unistd.h>
+#include <functional>
+#include <pthread.h>
+
+namespace ThreadModule
+{
+    template<typename T>
+    using func_t = std::function<void(T&)>;
+    // typedef std::function<void(const T&)> func_t;
+
+    template<typename T>
+    class Thread
+    {
+    public:
+        void Excute()
+        {
+            _func(_data);
+        }
+    public:
+        Thread(func_t<T> func, T& data, const std::string& name = "none-name")
+            : _func(func), _data(data), _threadname(name), _stop(true)
+        {}
+        static void* threadroutine(void* args) // 类成员函数，形参是有this指针的！！
+        {
+            Thread<T>* self = static_cast<Thread<T> *>(args);
+            self->Excute();
+            return nullptr;
+        }
+        bool Start()
+        {
+            int n = pthread_create(&_tid, nullptr, threadroutine, this);
+            if (!n)
+            {
+                _stop = false;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        void Detach()
+        {
+            if (!_stop)
+            {
+                pthread_detach(_tid);
+            }
+        }
+        void Join()
+        {
+            if (!_stop)
+            {
+                pthread_join(_tid, nullptr);
+            }
+        }
+        std::string name()
+        {
+            return _threadname;
+        }
+        void Stop()
+        {
+            _stop = true;
+        }
+        ~Thread() {}
+
+    private:
+        pthread_t _tid;
+        std::string _threadname;
+        T& _data;  // 为了让所有的线程访问同一个全局变量
+        func_t<T> _func;
+        bool _stop;
+    };
+} // namespace ThreadModule
+
+#endif
